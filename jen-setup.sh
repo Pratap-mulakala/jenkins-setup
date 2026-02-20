850d70eea0b6e30d72725cf1ca47271abfdad6e5
Pratap Mulakala <mulakalapratap777@gmail.com>
2026-02-20 11:23:01 +0530

    Update jen-setup.sh

--- jen-setup.sh
+++ jen-setup.sh
@@ -15,10 +15,29 @@ sudo mount --bind /var/tmp_disk /tmp
 echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
 sudo systemctl mask tmp.mount
 df -h /tmp
-sudo systemctl restart jenkins
+sudo systemctl restart jenkins  
+
+                 # ---------------------or-------------------------- another way for linux server
+#!/bin/bash
+
+sudo yum update -y
+# Add Jenkins repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# Import key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Install Java 17
+sudo yum install java-17-amazon-corretto -y
+# Install Jenkins and Git
+sudo yum install jenkins git -y
+# Enable and start Jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
 
 
-# setup the jenkins server while using ubuntu/debian os
+#---------- setup the jenkins server while using ubuntu/debian os------------------
 sudo apt update 
 sudo apt install openjdk-17-jdk
 java --version
